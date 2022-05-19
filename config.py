a599ee4e7e119923675cd81cf008fc01b5286898
Misigomartin <Misigomartin@gmail.com>
2022-05-19 10:17:33 +0300

    add a config file

---
+++ config.py
@@ -0,0 +1,43 @@
+import os
+
+class Config:
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:crazy@localhost/pitch'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class ProdConfig(Config):
+        SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+        debug = os.environ.get("DEBUG")
+
+
+class TestConfig(Config):
+   
+    pass
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://martin:Misigouts94@localhost:5432/blogsite'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
